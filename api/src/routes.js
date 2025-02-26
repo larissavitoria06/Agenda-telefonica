9789143e5e245f033d3197d85ccac7665ba7fb41
larissavitoria06 <160656461+larissavitoria06@users.noreply.github.com>
2025-02-26 16:22:34 -0300

    Update routes.js

--- api/src/routes.js
+++ api/src/routes.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const routes = express.Router();
 
-const Consulta = require('./controllers/agendamentos');
+const Telefones = require('./controllers/telefonicas');
+
 
 routes.get('/', (req, res) => {
-    res.json({ titulo: 'Agenda Telefonica Larissas'});
+    res.json({ titulo: 'Agenda de Telefones' });
 });
 
-routes.post('', Consulta.create);
-routes.get('/consultas', Consulta.read);
+routes.post('/telefones', Telefones.create);   
+routes.get('/telefones', Telefones.read); 
+routes.delete('/telefones/:telefone_id', Telefones.deletar); 
+routes.put('/telefones/:telefone_id', Telefones.update);     
+
+routes.use((req, res) => {
+    res.status(404).json({ erro: 'Rota não encontrada.' });
+});
 
 module.exports = routes;
